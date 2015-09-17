01f14352bad705ad17fd9b7d7eaf68d5bcdbe230
ruitian <chinabingwei@163.com>
2015-09-17 21:16:02 +0800

    add modifyAply

--- src/lib/servlet/Modifyservlet.java
+++ src/lib/servlet/Modifyservlet.java
@@ -1,7 +1,51 @@
 package lib.servlet;
 
+import lib.Dao.SqlCon;
+import lib.Dao.Userdao;
+import lib.Model.Apply;
+import lib.Model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+
 /**
  * Created by baron on 15-9-17.
  */
-public class Modifyservlet {
+@WebServlet(urlPatterns = "/modify", name = "modify")
+public class Modifyservlet extends HttpServlet{
+    SqlCon sqlCon = new SqlCon();
+    Userdao userdao = new Userdao();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String role = new String(req.getParameter("role").getBytes("ISO-8859-1"), "UTF-8");
+        String dataid = new String(req.getParameter("dataid").getBytes("ISO-8859-1"), "UTF-8");
+
+        User user = new User();
+        Apply apply = new Apply();
+        Connection con = null;
+
+        user.setDataid(dataid);
+        user.setRole(role);
+        apply.setDataid(dataid);
+
+        try {
+            con = sqlCon.getCon();
+            userdao.modify(con, user);
+            userdao.deleteapply(con, apply);
+            resp.sendRedirect("/admin/index.jsp");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
